f27cf65184793f33242f8ce732c41cfc8e32fadc
Elena Tikhonova <licose2019@gmail.com>
2020-08-18 23:44:15 +0300

    smth new

--- src/Panel.js
+++ src/Panel.js
@@ -40,7 +40,7 @@ export const Panel = () => {
               </div>
             </div>
             <div className="pl-lg-5 col-lg-9 mt-3">
-              <h2 className="hero-heading mb-0 font-weight-normal">Hello, I'm Elena!</h2>
+              <h2 className="hero-heading mb-0 font-weight-normal">Welcome to my page!</h2>
               <div className="text-block">
                 <div>
                   <p className="text-muted">
@@ -56,6 +56,23 @@ export const Panel = () => {
                   </p>
                 </div>
               </div>
+              <h3 className="hero-heading mb-0 font-weight-normal">What about me?</h3>
+              <div className="text-block">
+                <div>
+                  <p className="text-muted">
+                    Samsa was a travelling salesman - and above it there hung a
+                    picture that he had recently cut out of an illustrated
+                    magazine and housed in a nice, gilded frame.{" "}
+                  </p>
+                  <p className="text-muted">
+                    He must have tried it a hundred times, shut his eyes so that
+                    he wouldn't have to look at the floundering legs, and only
+                    stopped when he began to feel a mild, dull pain there that
+                    he had never felt before.{" "}
+                  </p>
+                </div>
+              </div>
+              <hr/>
               <div className="text-block">
                 <h4 className="mb-3">My projects</h4>
                 <div className="row">
